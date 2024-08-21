e1f198c1d087e1bd5ef54615933dba40a3afebcc
sheymabel <122784710+sheymabel@users.noreply.github.com>
2024-08-21 14:14:15 +0100

    gistion matrice c#

---
+++ GestionMatrice/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using TriMatrice.tri;
+
+GererMatrice  GestionnaireMatrice=new GererMatrice(5,5);
+GestionnaireMatrice.TraiterMatrice(MonAlgoDeTrie2, GestionnaireMatrice.Matrice);
+
+
+
+    int[] MonAlgoDeTrie(int[] t)
+{
+    return t;
+}
+int[] MonAlgoDeTrie2(int[] t)
+{
+    return t;
+}
